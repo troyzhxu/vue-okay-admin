f49e96fb032f9b08dc583cc87201756e22d5c711
ZhouXu <zhou.xu@ejlchina.com>
2021-07-05 19:51:32 +0800

    perf: 修改晚上时间判断
    (cherry picked from commit b5bb284d65d6351192f9de973d1b25d712d8107b)

--- src/hooks/web/useDarkMode.ts
+++ src/hooks/web/useDarkMode.ts
@@ -3,9 +3,9 @@ function isNowNight(): boolean {
   const month = now.getMonth();
   const hour = now.getHours();
   if (month > 4 && month <= 10) {
-    return hour < 5 || hour >= 21;
+    return hour < 6 || hour >= 19;
   }
-  return hour < 6 || hour >= 20;
+  return hour < 7 || hour >= 18;
 }
 
 export function useDarkMode() {

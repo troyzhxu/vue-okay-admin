4669f460ee9d6b8d9a2d1cb38edc979ced4d6317
ZhouXu <zhou.xu@ejlchina.com>
2021-07-07 10:58:24 +0800

    refactor: auth

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -14,13 +14,3 @@ export function getAuthCache<T>(key: BasicKeys) {
   const fn = isLocal ? Persistent.getLocal : Persistent.getSession;
   return fn(key) as T;
 }
-
-export function setAuthCache(key: BasicKeys, value) {
-  const fn = isLocal ? Persistent.setLocal : Persistent.setSession;
-  return fn(key, value, true);
-}
-
-export function clearAuthCache(immediate = true) {
-  const fn = isLocal ? Persistent.clearLocal : Persistent.clearSession;
-  return fn(immediate);
-}

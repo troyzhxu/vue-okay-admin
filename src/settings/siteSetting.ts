15f99c5c808b97fa54ac7416716b332dfc4f025c
ZhouXu <zhou.xu@ejlchina.com>
2021-07-07 17:10:40 +0800

    docs: siteSettings

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,5 +1,5 @@
 // github repo url
-export const GITHUB_URL = 'https://github.com/anncwb/vue-okay-admin';
+export const GITHUB_URL = 'https://github.com/ejlchina/vue-okay-admin';
 
 // vue-okay-admin-next-doc
 export const DOC_URL = 'https://vvbin.cn/doc-next/';

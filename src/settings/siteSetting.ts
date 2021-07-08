0cc3700286c2966ba5fe3f8df567281455de4b04
ZhouXu <zhou.xu@ejlchina.com>
2021-07-08 09:18:01 +0800

    wip: site_url

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -3,3 +3,5 @@ export const GITHUB_URL = 'https://github.com/ejlchina/vue-okay-admin';
 
 // vue-okay-admin-next-doc
 export const DOC_URL = 'https://vvbin.cn/doc-next/';
+
+export const SITE_URL = 'https://okhttps.ejlchina.com';
